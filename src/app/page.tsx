415f8c7275ffd1618b1cd92bd7ad711e5712ac8b
gabrielbueno <gabrielbueno@lugarh.com.br>
2025-04-08 08:56:51 -0300

    feat: add biome && initial text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <div>
-      
+      <h1 className="font-bold text-center mt-20 text-6xl max-w-96">
+        Write & Wire your ideas with <span className="text-violet-500">Anota AI</span>
+      </h1>
     </div>
-  );
+  )
 }
