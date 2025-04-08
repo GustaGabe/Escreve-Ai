da2331431f4f0e90e81bc34773d90b353763f452
gabrielbueno <gabrielbueno@lugarh.com.br>
2025-04-08 09:31:35 -0300

    feat: add more initial texts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
 export default function Home() {
   return (
     <div>
-      <h1 className="font-bold text-center mt-20 text-6xl max-w-96">
-        Write & Wire your ideas with <span className="text-violet-500">Anota AI</span>
-      </h1>
+      <div>
+        <h1 className="font-bold text-center mt-20 text-6xl max-w-96">
+          Write & Wire your ideas with <span className="text-violet-500">Anota AI</span>
+        </h1>
+      <p>Description</p>
+      </div>
+        <div>
+          <button className="border p-2 rounded-md">Button 1</button>
+          <button className="bg-gray-500 border border-gray-500 p-2 rounded-md">Button 2</button>
+        </div>
     </div>
   )
 }
