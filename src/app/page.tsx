c04d337d23ae9aaa455e5a3cee1038516c674b21
gabrielbueno <gabrielbueno@lugarh.com.br>
2025-04-08 11:30:28 -0300

    feat: add logo && adjust hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,44 @@
 import { Header } from '@/components/header'
+import { ArrowRight, SquareArrowOutUpRight } from 'lucide-react'
+import Image from 'next/image'
+import Link from 'next/link'
+import Hero from '../assets/full-logo.png'
 
 export default function Home() {
   return (
     <div>
       <Header />
       <main className="max-w-[1280px]">
-        <div>
-          <h1 className="font-bold text-center mt-20 text-6xl max-w-96">
-            Write & Wire your ideas with <span className="text-violet-500">Anota AI</span>
-          </h1>
-          <p>Description</p>
-        </div>
-        <div className='flex gap-4'>
-          <button type='submit' className="border p-2 rounded-md">Get Started</button>
-          <button type='submit' className="bg-gray-500 border border-gray-500 p-2 rounded-md">Button 2</button>
+        <div className="flex  items-center  ">
+          <div>
+            <h1 className="font-bold text-left mt-20 text-6xl max-w-120">
+              Write & Wire your ideas with <span className="text-violet-500">Anota AI</span>
+            </h1>
+            <p className="p-2 text-xl max-w-118">
+              Write, and visualize your ideas freely with Anota AI your personal space for thoughts
+              and creativity.
+            </p>
+            <div className="flex gap-4 mt-4">
+              <Link
+                href="#"
+                className="w-full max-w-[180px] gap-2 flex justify-center items-center shadow-md bg-[#0a0a0a] border border-violet-500 p-2 rounded-md text-white hover:bg-violet-500 ease-in-out duration-600"
+              >
+                Get Started
+                <ArrowRight />
+              </Link>
+              <Link
+                href="https://github.com/GustaGabe"
+                target="_blank"
+                className="w-full shadow-lg max-w-[180px] gap-2 flex justify-center items-center bg-violet-500 border border-violet-500 p-2 rounded-md hover:bg-violet-600"
+              >
+                View on Github <SquareArrowOutUpRight />
+              </Link>
+            </div>
+          </div>
+
+          <div className="ml-96">
+            <Image src={Hero} alt="hero" />
+          </div>
         </div>
       </main>
     </div>
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,12 +1,20 @@
-import { Notebook } from "lucide-react"
+import Logo from '../assets/logo.png'
+import Image from 'next/image'
 
 export const Header = () => {
   return (
-    <div className="border-b border-b-neutral-800 p-4">
+    <div className="border-b bg-[#0a0a0a] border-b-neutral-800 p-4">
       <div className="max-w-[1280px] mx-auto flex items-center justify-between">
-        <span className="font-bold flex gap-2 text-xl items-center"><Notebook /> Anota Ai</span>
+        <span className="font-bold flex gap-2 text-xl items-center">
+          <Image width={40} src={Logo} alt="Logo" /> Anota Ai
+        </span>
 
-        <button className="rounded-lg font-bold bg-violet-500 p-2 w-full max-w-[120px]" type="button">Sign in</button>
+        <button
+          className="rounded-lg font-bold bg-violet-500 p-2 w-full max-w-[120px]"
+          type="button"
+        >
+          Sign in
+        </button>
       </div>
     </div>
   )
