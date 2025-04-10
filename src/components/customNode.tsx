85b5b8cd5c55126a2a4c7776e8867690cd4008dd
Gusta <143643659+Gusstagabe@users.noreply.github.com>
2025-04-09 22:39:36 -0300

    feat: add node resizer

--- src/components/customNode.tsx
+++ src/components/customNode.tsx
@@ -1,9 +1,11 @@
-import { Handle, Position } from "@xyflow/react"
+import { Handle, NodeResizer, Position } from "@xyflow/react"
 
 export const CustomNode = ({ data }: any) => {
     return (
         <div className="px-4 py-2 shadow-md rounded-md bg-black border-2 border-violet-500">
+           <NodeResizer />
           <div className="flex">
+         
             <div className="rounded-full w-12 h-12 flex justify-center items-center text-white">
               {data.label}
             </div>
